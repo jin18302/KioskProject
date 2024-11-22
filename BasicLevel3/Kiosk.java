c487b8ebfd7f251a5efd3e673377439360b40042
lee sujin <jin18302@naver.com>
2024-11-23 00:25:55 +0900

    revise1

--- BasicLevel3/Kiosk.java
+++ BasicLevel3/Kiosk.java
@@ -3,25 +3,20 @@ package BasicLevel3;
 import java.util.*;
 
 public class Kiosk {
-    List<MenuItem> list = new ArrayList<>();
+    List<MenuItem> ItemList = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
 
     Kiosk(List<MenuItem> ItemList){
-        this.list=ItemList;
+        this.ItemList=ItemList;
     }
 
 
     public void start() {
-        list.add(new MenuItem("빅버거", 8500, "양상추, 토마토, 치즈, 쇠고기패티가 들어간 시그니처"));
-        list.add(new MenuItem("불고기버거", 3500, "양상추, 토마토, 치즈, 기본패티, 특제소스가 들어간 인기버거"));
-        list.add(new MenuItem("새우버거", 5500, "양상추, 토마토, 치즈, 통통한 새우패티"));
-        list.add(new MenuItem("치킨버거", 7500, "양상추, 토마토, 치즈, 통다리살 패티"));
-
 
         System.out.println("원하는 메뉴의 번호를 입력해주세요. 종료는 0");
         System.out.println("-------MENU------");
 
-        Iterator<MenuItem> it = list.iterator();
+        Iterator<MenuItem> it = ItemList.iterator();
 
         int i = 1;
         while (it.hasNext()) {
@@ -31,7 +26,7 @@ public class Kiosk {
 
         int input = sc.nextInt();
         if (1 <= input && input <= 4)
-            System.out.println(list.get(input - 1).GetItemIntroduce());
+            System.out.println(ItemList.get(input - 1).GetItemIntroduce());
         else if (input == 0) {
             System.exit(0);
         }else{
--- BasicLevel4/Main.java
+++
@@ -1,4 +0,0 @@
-package BasicLevel4;
-
-public class Main {
-}
