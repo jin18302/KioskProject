c5c9b1978ac00b7a3bf150d5d5050b5afcc694fe
lee sujin <jin18302@naver.com>
2024-11-26 15:46:47 +0900

    4_5 level Maintenance-related fixes

--- BasicLevel4_5/Kiosk.java
+++ BasicLevel4_5/Kiosk.java
@@ -6,6 +6,7 @@ public class Kiosk {
     private Scanner sc = new Scanner(System.in);
     private List<Menu> MenuList = new ArrayList<>();
 
+
     public void setMenuList(Menu... menu) {
         for (Menu m : menu) {
             MenuList.add(m);
@@ -24,16 +25,16 @@ public class Kiosk {
             System.out.println();
 
             int input = sc.nextInt();
-            if (1 <= input && input <= 3) {
-                System.out.println("-----------------------------" + MenuList.get(input - 1).GetCategory() + "------------------------   (뒤로가기1, 종료0)");
+            if (1 <= input && input <= MenuList.size()) {
+                System.out.println("-----------------------------" + MenuList.get(input - 1).GetCategory() + "------------------------   (뒤로가기5, 종료0)");
                 MenuList.get(input - 1).GetItemList();//카테고리별 메뉴출력
-            } else if (input == 0) System.exit(0);
+            } else if (input == 0) break;
 
 
             input = sc.nextInt();
-            if (1 <= input && input <= 4) {
+            if (1 <= input && input <= MenuList.get(input - 1).size) {
                 System.out.println(input + "번 을 선택하셨습니다");
-            } else if (input == 1) continue;
+            } else if (input == 5) continue;
             else if (input == 0) break;
         }
         System.exit(0);
--- BasicLevel4_5/Menu.java
+++ BasicLevel4_5/Menu.java
@@ -1,35 +1,31 @@
 package BasicLevel4_5;
 
 import java.util.*;
-// MenuItem 클래스를 List로 관리
 
-// List에 들어있는 MenuItem을 순차적으로 보여주는 함수
-// List를 리턴하는 함수
-
-// 구조에 맞게 함수를 선언해놓고 가져다 사용하세요.
 
 public class Menu {
     private String Category;
-    private List<MenuItem> MenuList;
+    private List<MenuItem> MenuItemList;
+    int size=MenuItemList.size();
 
     public Menu(String Category,List<MenuItem> itemList) {
         this.Category = Category;
-        this.MenuList=itemList;
+        this.MenuItemList=itemList;
     }
 
-    public void SetMenuList(MenuItem item) {
-        this.MenuList.add(item);
+    public void SetItemList(MenuItem item) {
+        this.MenuItemList.add(item);
     }
 
     public void GetItemList() {
         int i=1;
-        Iterator<MenuItem> it =  MenuList.iterator();
+        Iterator<MenuItem> it =  MenuItemList.iterator();
         while (it.hasNext()){
             System.out.print((i++)+". "+it.next().GetItemIntroduce());
         }
     }
 
-    String GetCategory() {
+    public String GetCategory() {
         return Category;
     }
 
